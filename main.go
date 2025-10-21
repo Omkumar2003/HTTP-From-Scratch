d03999dc85799c914d6a4be9dce182e8a9984c35
Omkumar2003 <omkumar742003@gmail.com>
2025-10-21 15:16:23 +0530

    ch1 l5

--- main.go
+++ main.go
@@ -2,31 +2,58 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
 
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+
+	go func() {
+
+		defer f.Close()
+		// ob := make([]byte, 8)
+		ob := make([]byte, 1)
+		// fmt.Print("read :")
+		// ch <- "read :"
+		str := ""
+		for {
+			_, err := f.Read(ob)
+			if err != nil {
+				ch <- str
+				break
+			}
+			if string(ob) == string('\n') {
+				// fmt.Println("")
+				str += string(ob)
+				ch <- str
+				str = ""
+				// ch <- "read :"
+				continue
+			} else {
+				str += string(ob)
+			}
+
+			// fmt.Print(string(ob[:ok]))
+			// ch <- string(ob)
+		}
+		close(ch)
+	}()
+
+
+	return ch
+}
+
 func main() {
+
 	f, err := os.Open("messages.txt")
 	if err != nil {
 		log.Panic(err)
 	}
 
-	defer f.Close()
-	// ob := make([]byte, 8)
-	ob := make([]byte, 1)
-	fmt.Print("read :")
-
-	for {
-		ok, err := f.Read(ob)
-		if err != nil {
-			break
-		}
-		if string(ob) == string('\n') {
-			fmt.Println("")
-			fmt.Print("read :")
-			continue
-		}
-		fmt.Print(string(ob[:ok]))
+	chg := getLinesChannel(f)
+	for m := range chg {
+		fmt.Print("read :" + m)
 	}
 }
